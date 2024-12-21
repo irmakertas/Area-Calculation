a6d75e3a12c4664370637826d595f572ca15ef95
Irmak Ertaş <irmakertas8@gmail.com>
2024-12-21 09:46:29 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,11 @@ package polymorphism.area;
 
 public class Main {
     public static void main(String[] args) {
+        AreaCalculator calculate = new AreaCalculator();
 
-        AreaCalculator alanhesaplama = new AreaCalculator();
-
-        alanhesaplama.alanHesapla();
-        alanhesaplama.alanHesapla(5);
-        alanhesaplama.alanHesapla(7,3);
-
+        calculate.calculateArea();
+        calculate.calculateArea(5);
+        calculate.calculateArea(7,3);
+        calculate.calculateArea(7.5, 6.7);
     }
 }
